bfb2dbd1fe7460f4e0c469b73ecf84190229306c
saieshwarinduri <indurisaieshwar225@gmail.com>
2023-02-06 13:49:49 +0530

    last commit

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-let h1 = document.getElementById("heading");
-let hourstick = document.getElementById("hour");
-let minuteStick = document.getElementById("min");
-let secStick = document.getElementById("sec");
-setInterval(() => {
-  let d = new Date();
-  let min = d.getMinutes();
-  let hour = d.getHours();
-  let sec = d.getSeconds();
 
-  let hr_rotate = 30 * hour + min / 2;
-  let min_rotate = 6 * min;
-  let sec_rotate = 6 * sec;
-
-  hourstick.style.transform = `rotate(${hr_rotate}deg)`;
-  minuteStick.style.transform = `rotate(${min_rotate}deg)`;
-  secStick.style.transform = `rotate(${sec_rotate}deg)`;
-}, 1000);
